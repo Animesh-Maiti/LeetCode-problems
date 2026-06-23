44264bd82305ced26acebb64981458322182a92e
Animesh <animeshmaiti862@gmail.com>
2026-06-24 00:12:05 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (23.63%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -3,8 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         int number=nums[0];
         for(int i=1;i<nums.size();i++){
-            number=number^nums[i];
-        }
+            number=number^nums[i];}
         return number; 
     }
 };
